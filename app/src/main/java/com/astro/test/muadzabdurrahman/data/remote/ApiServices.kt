c157da12c4166678b31dac05cf57e8bacab7c1f3
Mu'adz <muuadzz10@gmail.com>
2022-09-10 22:44:29 +0700

    add base api service class

---
+++ app/src/main/java/com/astro/test/muadzabdurrahman/data/remote/ApiServices.kt
@@ -0,0 +1,43 @@
+package com.astro.test.muadzabdurrahman.data.remote
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+interface ApiServices {
+
+    companion object {
+
+        private const val BASE_URL = "https://api.github.com/"
+
+        private val gson = GsonBuilder()
+            .setPrettyPrinting()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+
+        private val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        private val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+        fun create() : ApiServices {
+            return Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .build()
+                .create(ApiServices::class.java)
+
+        }
+    }
+}

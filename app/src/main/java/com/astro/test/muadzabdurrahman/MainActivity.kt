25b99891eaee760bd6cb74e080726ff47ee289ec
Mu'adz <muuadzz10@gmail.com>
2022-09-10 16:40:40 +0700

    update main activity class

--- app/src/main/java/com/astro/test/muadzabdurrahman/MainActivity.kt
+++ app/src/main/java/com/astro/test/muadzabdurrahman/MainActivity.kt
@@ -2,10 +2,14 @@ package com.astro.test.muadzabdurrahman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.astro.test.muadzabdurrahman.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        supportActionBar?.subtitle = getString(R.string.astro_test)
     }
 }

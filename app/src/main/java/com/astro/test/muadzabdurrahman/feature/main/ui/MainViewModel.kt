5756f4dfdc468693c022a357a94d890bbf76d56f
Mu'adz <muuadzz10@gmail.com>
2022-09-11 18:42:58 +0700

    update main viewmodel with search user variables

--- app/src/main/java/com/astro/test/muadzabdurrahman/feature/main/ui/MainViewModel.kt
+++ app/src/main/java/com/astro/test/muadzabdurrahman/feature/main/ui/MainViewModel.kt
@@ -13,9 +13,21 @@ class MainViewModel : ViewModel() {
     private val dataSource = SearchDataSource(service)
     private val repository: SearchRepository = SearchRepositoryImpl(dataSource)
 
+    var keyword: String = ""
+    var page: Int = 1
+    private var _userList : MutableList<User> = mutableListOf()
+    var userList: List<User> get() = _userList
+        set(value) {
+            _userList = value.toMutableList()
+        }
+
+    fun addUserList(users : List<User>) {
+        _userList.addAll(users)
+    }
+
     val searchEventManager: StateEventManager<List<User>> = repository.searchStateEventManager
 
-    fun searchUser(keyword: String, page: Int) {
+    fun searchUser() {
         repository.search(keyword, page)
     }
 }
